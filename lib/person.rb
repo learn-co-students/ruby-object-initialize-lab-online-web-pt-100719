edeb29ae58080f5903a195a5b881a43d3444d7ae
Jeff Eckert <bromodo@gmail.com>
2019-10-24 20:59:51 -0400

    Done.

---
+++ lib/person.rb
@@ -0,0 +1,28 @@
+# class Person
+#     def initialize(name)
+#       @name = name
+#     end
+   
+#     def name=(name)
+#       @name = name
+#     end
+   
+#     def name
+#       @name
+#     end
+#   end
+
+class Person
+    def initialize (persons_name)
+      @name = persons_name
+    end
+  
+    def name=(persons_name)
+      @name = persons_name
+    end
+  
+    def name
+      @name
+    end
+    
+  end
