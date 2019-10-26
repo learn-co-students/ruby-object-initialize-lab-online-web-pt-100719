7eea9a23850046e387f73a8e910c15a6dc10777c
Anika Christopher <anika.mchristopher@gmail.com>
2019-10-26 02:44:13 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,38 @@
+class Dog
+  
+  def initialize(name) 
+    @name = name
+  end
+  
+  def name=(name)
+    @name = name
+  end
+  
+  def name
+    @name
+  end
+  
+  def initialize(dogs_name, dogs_breed="Mutt")
+    @name = dogs_name
+    @breed = dogs_breed 
+  end
+  
+  def name=(name)
+    @name = name
+  end
+    
+  def breed=(breed)
+    @breed = breed
+  end
+    
+  def name
+    @name
+  end
+    
+  def breed
+    @breed
+  end
+    
+end  
+  
+  
